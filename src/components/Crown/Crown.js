8008a0dc9265f2c7d0c142b5abaf854aee1f5baf
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-06-22 17:29:32 -0600

    added content to symptoms section in crown

--- src/components/Crown/Crown.js
+++ src/components/Crown/Crown.js
@@ -20,12 +20,32 @@ class Crown extends Component {
                 <ChakraTitle title="Crown Chakra"/>
 
                     <h2>sahasrara</h2>
-                    <p>The crown or the seventh chakra is located at the top of the head and is our connection to the Universe.<br/>In fact, the Seventh Chakra disperses the Universal energy or life force into the six other chakras located below it. In Sanskrit, the Crown Chakra is termed as Sahasrara,<br/> the ‘thousand fold’ Chakra, since it is pictured as a lotus having a thousand white petals.</p>
+                    <p>The crown or the seventh chakra is located at the top of the head and is our connection to the Universe.<br/>In fact, the Seventh Chakra disperses the Universal energy or life force into the six other chakras located below it. In Sanskrit, the <br/>Crown Chakra is termed as Sahasrara, the ‘thousand fold’ Chakra, since it is pictured as a lotus having a thousand white petals.</p>
                 </section>
 
                 <section className='symptoms'>
-                    <div>balanced</div>
-                    <div>unbalanced</div>
+                    <div className='balanced'>
+                        <h1>CLOSED</h1>
+                        <ul>
+                            <li>depression</li>
+                            <li>inability to learn</li>
+                            <li>disconnected</li>
+                            <li>prejudiced</li>
+                            <li>confused</li>
+                            <li>feeling incomplete</li>
+                        </ul>
+                    </div>
+                    <div className='unbalanced'>
+                        <h1>OPEN</h1>
+                        <ul>
+                            <li>presence/mindfulness</li>
+                            <li>unshakeable trust in our inner guidence</li>
+                            <li>mystical oneness</li>
+                            <li>peacefulness</li>
+                            <li>clarity</li>
+                            <li>access to universal consciousness</li>
+                        </ul>
+                    </div>
                 </section>
                 
                 <section className='guidance'>
